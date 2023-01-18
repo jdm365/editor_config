5d10239b3ec006905fdebcff296dfd0a15bf24bb
jdm365 <jdm365@georgetown.edu>
2023-01-18 14:14:58 -0500

    Fix

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # editor_config
 Editor config for new device.
+
+For Mac:
+Run script and type `:PlugInstall` once entered into config.
--- mac_install.sh
+++ mac_install.sh
@@ -1,2 +1,4 @@
 brew install neovim
+cp init.vim ~/.config/nvim/init.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim ~/.config/nvim/init.vim
